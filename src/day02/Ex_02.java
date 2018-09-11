3c243f2936d3ba33f74b69ce944f1ed308fa3236
theqoeo <theqoeo@naver.com>
2018-09-11 16:29:20 +0900

    403 1

---
+++ src/day02/Ex_02.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class Ex_02 {
+
+	public static void main(String[] args) {
+		System.out.println("JJJJJJJJJJ    AAAAAAA     VV     VV     AAAAAAA");
+		System.out.println("JJJJJJJJJJ   AAAAAAAAAA    V      V    AAAAAAAAA");
+		System.out.println("       JJJ  AAA      AAA   VV    VV   AAA     AAA");
+		System.out.println("       JJJ  AAA       AAA   V    V    AAA     AAA");
+		System.out.println("       JJJ AAAAAAAAAAAAAAA   V  V    AAAAAAAAAAAAA");
+		System.out.println("J     JJJ  AAAAAAAAAAAAAAA    VV     AAAAAAAAAAAAA");
+		System.out.println("JJ    JJJ  AAA         AAA    VV     AAA       AAA");
+		System.out.println(" JJJJJJ   AAA           AAA    V    AAA         AAA");
+		System.out.println("  JJJJ   AAA             AAA   V     AAA        AAA");
+	}
+
+}
---
+++ src/day02/Intro.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요? 컴공 2학년 이름은 배대영이라고 합니다.");
+		System.out.println("저는 자바를 처음 공부합니다. ");
+		System.out.println("저의 나이는 24살입니다.");
+	}
+
+}
